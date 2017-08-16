0ea702bf6e192ddaa0979b4f196deedce6dfd6a1
hilary01 <zhanglijie@rdgchina.com>
2017-08-16 16:25:39 +0800

    youyi

--- YouYiBook/index.android.js
+++ YouYiBook/index.android.js
@@ -9,29 +9,101 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  Image,
+  TouchableOpacity,
+  ViewPagerAndroid,
+  ScrollView,
+  Navigator,
+  View,
+  ListView,
+  Dimensions,
+  WebView,
+  ToastAndroid,
+  Button,
+  DrawerLayoutAndroid,
+
 } from 'react-native';
 
-export default class YouYiBook extends Component {
+import SideMenu from 'react-native-side-menu';
+import Menu from './app/activity/menu'; //导入菜单组件
+
+const uri_image_menu = 'http://image18-c.poco.cn/mypoco/myphoto/20160605/09/17351665220160605093956066.png';
+
+const { width, height } = Dimensions.get('window');
+import Toast, { DURATION } from 'react-native-easy-toast';
+class YouYiBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      selectedItem: 'About',
+    };
+  }
+
+  toggle() {
+    this.refs.toast.show('toggle',1000);
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  updateMenuState(isOpen) {
+    this.setState({
+      isOpen: isOpen
+    });
+  }
+
+  onMenuItemSelected = (item) => {
+    this.setState({
+      isOpen: false,
+      selectedItem: item,
+    });
+  }
+  showToast() {
+
+    this.refs.toast.show(this.state.selectedItem,1000);
+  }
   render() {
+    const menu = <Menu onItemSelected={this.onMenuItemSelected} />
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <SideMenu
+        menu={menu}
+        isOpen={this.state.isOpen}
+        openMenuOffset={width/4*3}
+        onChange={(isOpen) => this.updateMenuState(isOpen)}
+      >
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
+            </Text>
+          <Text style={[styles.instructions, { color: 'red' }]} onPress={() => this.showToast(this.state.selectedItem)}>
+            当前选中的菜单是: {this.state.selectedItem}
+          </Text>
+        </View>
+
+        <Button style={styles.button} onPress={() => this.toggle()} title="我是button，点击打开侧边栏" >
+          <Image
+            source={{ uri: uri_image_menu, width: 32, height: 32, }} />
+        </Button>
+        <Toast ref="toast" />
+      </SideMenu>
     );
   }
 }
 
 const styles = StyleSheet.create({
+
+  button: {
+    position: 'absolute',
+    top: 20,
+    padding: 10,
+  },
+  caption: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
